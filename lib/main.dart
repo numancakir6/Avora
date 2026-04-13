602442d9f97f19167d1584f78e9d109006050bdf
numancakir6 <numancakir6@gmail.com>
2026-04-13 19:41:30 +0300

    lexion

--- lib/main.dart
+++ lib/main.dart
@@ -17,46 +17,7 @@ class AvoraApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.black,
         primaryColor: Colors.white,
       ),
-      home: const LexionIntroPage(),
-    );
-  }
-}
-
-class LexionIntroPage extends StatefulWidget {
-  const LexionIntroPage({super.key});
-
-  @override
-  State<LexionIntroPage> createState() => _LexionIntroPageState();
-}
-
-class _LexionIntroPageState extends State<LexionIntroPage> {
-  @override
-  void initState() {
-    super.initState();
-
-    Future.delayed(const Duration(milliseconds: 1200), () {
-      if (!mounted) return;
-      Navigator.of(
-        context,
-      ).pushReplacement(MaterialPageRoute(builder: (_) => const SplashPage()));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          'LEXION',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 34,
-            fontWeight: FontWeight.w700,
-            letterSpacing: 8,
-          ),
-        ),
-      ),
+      home: const SplashPage(),
     );
   }
 }
